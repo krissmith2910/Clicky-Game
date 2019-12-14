8fef1f4dc11561b09db86235a3fee15ed71c4092
Kristopher Smith <ksmith2910@outlook.com>
2019-12-13 18:17:25 -0700

    Updates

---
+++ precache-manifest.911c953121748d75ac2fb4eff6f75d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8241c3a9930af4ef4e18b9cb2c2ebd2f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "8da384cb07ae7039f338",
+    "url": "/Clicky-Game/static/css/main.6f17afb9.chunk.css"
+  },
+  {
+    "revision": "7a48bee8ff02709a60c5",
+    "url": "/Clicky-Game/static/js/2.adc075e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.adc075e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8da384cb07ae7039f338",
+    "url": "/Clicky-Game/static/js/main.f8b302d9.chunk.js"
+  },
+  {
+    "revision": "e6311db2cb5e070b7b61",
+    "url": "/Clicky-Game/static/js/runtime-main.4cbabcab.js"
+  }
+]);
