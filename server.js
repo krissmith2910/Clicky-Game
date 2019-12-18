573d96cba33634bdb1ef1ef9e50e987d5d4f16aa
Kristopher Smith <ksmith2910@outlook.com>
2019-12-18 12:25:15 -0700

    refactor

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //server.js
 console.log("start")
 const express = require('express');
+const path = require("path");
 console.log("express")
 //const favicon = require('express-favicon');
-const path = require('path');
 console.log("express")
 const port = process.env.PORT || 3000;
 console.log("express")
@@ -12,17 +12,16 @@ const router = express.Router()
 console.log("express")
 //app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-console.log("static1")
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 // app.use(express.static(path.join(__dirname, 'build')));
 // console.log("static2")
 // app.get('/ping', function (req, res) {
 //  return res.send('pong');
 // });
-router.use('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 console.log("route")
 app.listen(port,function() {
-  console.log("app listening")
+  console.log("app listening on port " + port)
 });
